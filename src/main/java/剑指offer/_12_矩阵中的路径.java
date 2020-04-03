615943f223887d3a2565d06bae866e957008c311
hanbing <hanb16@dimpt.com>
2020-04-03 19:08:59 +0800

    add

---
+++ src/main/java/剑指offer/_12_矩阵中的路径.java
@@ -0,0 +1,41 @@
+package 剑指offer;
+
+/**
+ * @author hanbing
+ * @create 2020-04-03 18:04
+ */
+
+//https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/
+public class _12_矩阵中的路径 {
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+        boolean[][] visited = new boolean[rows][cols]; //存储是否访问过
+        for (int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++){
+                if(existCore(i, j, board, word, visited, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean existCore(int i, int j, char[][] board, String word, boolean[][] visited, int indexOfWord) {
+        if (indexOfWord == word.length()) return true;
+        if (i<0 || i>=board.length) return false;
+        if (j<0 || j>=board[0].length) return false;
+        if (visited[i][j] == true) return false;
+        if (board[i][j] != word.charAt(indexOfWord)) return false;
+        visited[i][j] = true;
+        if(existCore(i+1, j, board, word, visited, indexOfWord+1)
+           || existCore(i-1, j, board, word, visited, indexOfWord+1)
+           || existCore(i, j+1, board, word, visited, indexOfWord+1)
+           || existCore(i, j-1, board, word, visited, indexOfWord+1)){
+            return true;
+        }else {
+            visited[i][j] = false;
+            return false;
+        }
+    }
+}
---
+++ src/main/java/剑指offer/_13_机器人的运动范围.java
@@ -0,0 +1,50 @@
+package 剑指offer;
+
+import sun.print.SunMinMaxPage;
+
+/**
+ * @author hanbing
+ * @create 2020-04-03 18:44
+ */
+
+//https://leetcode-cn.com/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof/
+public class _13_机器人的运动范围 {
+    public int movingCount(int m, int n, int k) {
+        boolean[][] visited = new boolean[m][n];
+        moving(0, 0, m, n, k, visited);
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++){
+                if (visited[i][j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void moving(int i, int j, int m, int n, int k, boolean[][] visited) {
+        if (i<0 || i>=m) return;
+        if (j<0 || j>=n) return;
+        if (gsum(i, j) > k) return;
+        if (visited[i][j]) return;
+        visited[i][j] = true;
+        moving(i+1, j, m, n, k, visited);
+        moving(i-1, j, m, n, k, visited);
+        moving(i, j+1, m, n, k, visited);
+        moving(i, j-1, m, n, k, visited);
+    }
+
+    private int gsum(int i, int j) {
+        int sum = 0;
+        while (i>0){
+            sum += (i%10);
+            i/=10;
+        }
+        while (j>0){
+            sum += (j%10);
+            j/=10;
+        }
+        return sum;
+    }
+}
