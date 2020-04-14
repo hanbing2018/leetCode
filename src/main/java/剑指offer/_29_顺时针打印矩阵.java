a3d3787b4e6c50408f56228d4f9355ba71229e14
hanbing <hanb16@dimpt.com>
2020-04-14 17:06:10 +0800

    add

---
+++ src/main/java/剑指offer/_29_顺时针打印矩阵.java
@@ -0,0 +1,46 @@
+package 剑指offer;
+
+/**
+ * @author hanbing
+ * @create 2020-04-14 14:39
+ */
+
+//https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+public class _29_顺时针打印矩阵 {
+    public int[] spiralOrder(int[][] matrix) {
+        int cows = matrix.length;
+        if (cows==0) return new int[0];
+        int cols = matrix[0].length;
+        int[] array = new int[cows*cols];
+        int start = 0;
+        int k = 0;
+        while (start*2<cows && start*2<cols){
+            //从左到右
+            for (int i = start; i <= cols-1-start; i++) {
+                array[k++] = matrix[start][i];
+            }
+            //从上到下
+            if (cows-1-start > start){
+                for (int i = start+1; i <= cows-1-start; i++) {
+                    array[k++] = matrix[i][cols-1-start];
+                }
+            }
+            //从右到左
+            if (cols-1-start>start && cows-1-start>start){
+                for (int i = cols-2-start; i >= start; i--) {
+                    array[k++] = matrix[cows-1-start][i];
+                }
+            }
+            //从下到上
+            if (cols-1-start>start && cows-2-start>start){
+                for (int i = cows-2-start; i >= start+1; i--) {
+                    array[k++] = matrix[i][start];
+                }
+            }
+
+
+            start++;
+        }
+        return array;
+    }
+}
---
+++ src/main/java/剑指offer/_30_包含min函数的栈.java
@@ -0,0 +1,40 @@
+package 剑指offer;
+
+import java.util.Stack;
+
+/**
+ * @author hanbing
+ * @create 2020-04-14 15:48
+ */
+
+//https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof/
+public class _30_包含min函数的栈 {
+    private Stack<Integer> stack;
+    private Stack<Integer> stackMin;  //存储当前的最小值
+
+    /** initialize your data structure here. */
+    public _30_包含min函数的栈() {
+        stack = new Stack<>();
+        stackMin = new Stack<>();
+    }
+
+    public void push(int x) {
+        if (stackMin.isEmpty() || stackMin.peek()>=x){
+            stackMin.push(x);
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        int pop = stack.pop();
+        if (pop==stackMin.peek()) stackMin.pop();
+    }
+
+    public int top() {
+       return stack.peek();
+    }
+
+    public int min() {
+        return stackMin.peek();
+    }
+}
