c880a1bc779bf8066d59eb33bebc21b77a581ede
hanbing <hanb16@dimpt.com>
2020-04-17 14:03:46 +0800

    add

---
+++ src/main/java/剑指offer/_33_二叉搜索树的后序遍历序列.java
@@ -0,0 +1,37 @@
+package 剑指offer;
+
+import sun.net.www.http.PosterOutputStream;
+
+import java.beans.EventHandler;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author hanbing
+ * @create 2020-04-17 11:59
+ */
+
+//https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/
+public class _33_二叉搜索树的后序遍历序列 {
+    public boolean verifyPostorder(int[] postorder) {
+        if (postorder==null) return false;
+        if (postorder.length == 0) return true;
+        return veryfiPostorderCore(postorder, 0, postorder.length-1);
+    }
+
+    private boolean veryfiPostorderCore(int[] postorder, int start, int end) {
+        if (end-start <= 1) return true;
+        int index = start;
+        while (index<end){
+            if (postorder[index] < postorder[end]) {
+                index++;
+            }else break;
+        }
+        int indexR = index;  //此时index只想第一个比末尾数大的数
+        while (indexR<end){
+            if (postorder[indexR] > postorder[end]){
+                indexR++;
+            }else return false;
+        }
+        return veryfiPostorderCore(postorder, start, index-1) && veryfiPostorderCore(postorder, index, end-1);
+    }
+}
---
+++ src/main/java/剑指offer/_34_二叉树中和为某一值的路径.java
@@ -0,0 +1,35 @@
+package 剑指offer;
+
+import 链表.ListNode;
+
+import java.time.temporal.ValueRange;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author hanbing
+ * @create 2020-04-17 12:44
+ */
+
+//https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/
+public class _34_二叉树中和为某一值的路径 {
+    LinkedList<List<Integer>> lists = new LinkedList<>();
+    LinkedList<Integer> list = new LinkedList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        pathSumCore(root, sum);
+        return lists;
+    }
+
+    private void pathSumCore(TreeNode root, int sum) {
+        if (root == null) return;
+        list.add(root.val);
+        sum-=root.val;
+        if (root.left==null && root.right==null && sum==0){
+            lists.add(new LinkedList<>(list));
+        }
+        pathSum(root.left, sum);
+        pathSum(root.right, sum);
+        list.removeLast();
+    }
+
+}
