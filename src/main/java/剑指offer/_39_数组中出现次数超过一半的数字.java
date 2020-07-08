776dec9947ea4aa178007c2b8c9d446a0a4dcc04
hanbing <hanb16@dimpt.com>
2020-07-08 13:05:36 +0800

    change

---
+++ src/main/java/剑指offer/_39_数组中出现次数超过一半的数字.java
@@ -0,0 +1,19 @@
+package 剑指offer;
+
+
+
+//https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/
+public class _39_数组中出现次数超过一半的数字 {
+
+    public int majorityElement(int[] nums) {
+
+        //摩尔投票法,也可以用HashMap,数值为key，出现的数字为value
+        int vote = 0;
+        int x = nums[0];
+        for (int num : nums) {
+            if (vote==0) x = num;
+            vote+= x==num? 1:-1;
+        }
+        return x;
+    }
+}
