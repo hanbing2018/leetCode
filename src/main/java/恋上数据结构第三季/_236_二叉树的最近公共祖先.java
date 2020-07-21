137b346a1fd284658416ff8202d09188dd4612a1
hanbing <hanb16@dimpt.com>
2020-07-21 21:53:10 +0800

    change

---
+++ src/main/java/恋上数据结构第三季/_236_二叉树的最近公共祖先.java
@@ -0,0 +1,16 @@
+package 恋上数据结构第三季;
+
+
+//https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
+public class _236_二叉树的最近公共祖先 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root==null || root==p || root==q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left!=null && right!=null) return root;
+
+        return left==null? right : left;
+    }
+}
