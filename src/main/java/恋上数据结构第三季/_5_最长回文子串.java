4553178de7c77fcc9b975ab9ac665c3c0ceb1d16
hanbing_2016 <hanbing_2016@163.com>
2020-08-26 19:19:35 +0800

    change

--- src/main/java/恋上数据结构第三季/_5_最长回文子串.java
+++ src/main/java/恋上数据结构第三季/_5_最长回文子串.java
@@ -5,7 +5,7 @@ package 恋上数据结构第三季;
 public class _5_最长回文子串 {
 
     //本题是经典面试题！有多种优秀的方法
-    //方法一：动态规划
+    //方法一：动态规划。相比暴力法O(n^3),动态规划以空间换时间，时间复杂度为O(n^2)
     public String longestPalindromeDp(String s) {
         if (s == null) return null;
         char[] cs = s.toCharArray();
@@ -14,6 +14,8 @@ public class _5_最长回文子串 {
         int maxLen = 1;
         // 最长回文子串的开始索引
         int begin = 0;
+
+        //bool数组dp[i][j]表示cs[i]到cs[j]是否是回文串
         boolean[][] dp = new boolean[cs.length][cs.length];
         // 从下到上（i由大到小）
         for (int i = cs.length - 1; i >= 0; i--) {
