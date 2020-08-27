bcb4f12640958411fe9022005710ec34437702a6
hanbing <hanb16@dimpt.com>
2020-08-27 09:18:56 +0800

    change

--- src/main/java/剑指offer/_11_旋转数组中的最小数字.java
+++ src/main/java/剑指offer/_11_旋转数组中的最小数字.java
@@ -7,13 +7,20 @@ package 剑指offer;
 
 //https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/
 public class _11_旋转数组中的最小数字 {
+
+    /**
+     * 二分查找的变形，时间复杂度为O(logN)
+     * @param numbers
+     * @return
+     */
     public int minArray(int[] numbers) {
         int left = 0;
         int right = numbers.length - 1;
-        int mid = left;
+        int mid = left;  //设置mid为最左的数，当数组并未旋转时直接返回left
         while (numbers[left]>=numbers[right]){
-            if (right-left == 1) return numbers[right];
+            if (right-left == 1) return numbers[right]; //right与left相差1，此时right指向左部分最大值，left指向右部分最小值
             mid = (left + right)/2;
+            //如果left、right和mid指向的数值相等，则不能判断最小值的位置，此时要采用顺序查找的方式
             if (numbers[left]==numbers[mid] && numbers[mid]==numbers[right]){
                 return minEqual(numbers);
             }
