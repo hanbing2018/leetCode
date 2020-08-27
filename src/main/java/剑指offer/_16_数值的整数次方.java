4d4fd30a7ccb49c57384a32217ac373cd962d1f7
hanbing <hanb16@dimpt.com>
2020-08-27 17:14:32 +0800

    change

--- src/main/java/剑指offer/_16_数值的整数次方.java
+++ src/main/java/剑指offer/_16_数值的整数次方.java
@@ -7,6 +7,14 @@ package 剑指offer;
 
 //https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof/
 public class _16_数值的整数次方 {
+
+    /**
+     * 通过测试得知，原题中并没有考虑x为0的情况，所以下面的解法也通过了
+     * 需要注意的是考虑n小于0的情况
+     * @param x
+     * @param n
+     * @return
+     */
     public double myPow(double x, int n) {
         if (n >= 0){
             return myPowUnsigned(x, n);
