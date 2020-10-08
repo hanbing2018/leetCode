5442faf56752e67f397170b651249fc1effb514f
hanbing_2016 <hanbing_2016@163.com>
2020-10-08 18:44:35 +0800

    y

--- src/main/java/剑指offer/_29_顺时针打印矩阵.java
+++ src/main/java/剑指offer/_29_顺时针打印矩阵.java
@@ -15,33 +15,33 @@ public class _29_顺时针打印矩阵 {
      * @return
      */
     public int[] spiralOrder(int[][] matrix) {
-        int cows = matrix.length;
-        if (cows == 0) return new int[0];
+        int rows = matrix.length;
+        if (rows == 0) return new int[0];
         int cols = matrix[0].length;
-        int[] array = new int[cows * cols];
+        int[] array = new int[rows * cols];
         int start = 0;
         int k = 0;
-        //循环的条件是开始的位置start * 2 < cows && start * 2 < cols
-        while (start * 2 < cows && start * 2 < cols) {
+        //循环的条件是开始的位置start * 2 < rows && start * 2 < cols
+        while (start * 2 < rows && start * 2 < cols) {
             //从左到右
             for (int i = start; i <= cols - 1 - start; i++) {
                 array[k++] = matrix[start][i];
             }
             //从上到下
-            if (start < cows - 1 - start) {
-                for (int i = start + 1; i <= cows - 1 - start; i++) {
+            if (start < rows - 1 - start) {
+                for (int i = start + 1; i <= rows - 1 - start; i++) {
                     array[k++] = matrix[i][cols - 1 - start];
                 }
             }
             //从右到左
-            if (start < cols - 1 - start && start < cows - 1 - start) {
+            if (start < cols - 1 - start && start < rows - 1 - start) {
                 for (int i = cols - 2 - start; i >= start; i--) {
-                    array[k++] = matrix[cows - 1 - start][i];
+                    array[k++] = matrix[rows - 1 - start][i];
                 }
             }
             //从下到上
-            if (start < cols - 1 - start && start < cows - 2 - start) {
-                for (int i = cows - 2 - start; i >= start + 1; i--) {
+            if (start < cols - 1 - start && start < rows - 2 - start) {
+                for (int i = rows - 2 - start; i >= start + 1; i--) {
                     array[k++] = matrix[i][start];
                 }
             }
